25d9a2124fc176c314d36d1c9eb820325e537114
User <User@GUUCOMP-0394.ad.guu.ru>
2017-11-08 18:39:48 +0300

    ee

--- hello world/src/Circle.java
+++ hello world/src/Circle.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 
-public class Circle {
+public class Circle extends Fig {
+	public void showfig() {
+		System.out.print("Circle with radius:");
+	};
 
+	public void input() {
+		System.out.print("Type Circle radius: ");
+		Scanner in = new Scanner(System.in);
+	};
 }
