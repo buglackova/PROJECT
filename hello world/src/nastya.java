3cc9829c813e258eba29d13ec85315943e862664
lenakaminskaya <kaminskaya1016@gmail.com>
2017-11-08 18:05:11 +0300

    234

--- hello world/src/nastya.java
+++ hello world/src/nastya.java
@@ -9,7 +9,7 @@ public class nastya {
 		Fig h;
 		Scanner in = new Scanner(System.in);
 
-		while (true) {
+		while (true) { 
 			System.out
 					.println("\n0-exit; 1-add circle; 2-add sqare; 3-add triangle; 4-show all figures; 5-change figure: ; 6-delete last figure");
 			k = in.nextInt();
